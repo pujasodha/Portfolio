77af78a635de9d877b21a6afe52946ed44e0fe22
pujasodha <pujasodha@gmail.com>
2019-11-09 19:17:52 -0800

    updated 11/9

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,38 @@
-
-
-  $(document).ready(function(){
-//page navigation
+$(document).ready(function() {
+    //page navigation
     $('.scrollspy').scrollSpy();
 
-//nav bar trigger and function
+    //nav bar trigger and function
     $('.sidenav').sidenav();
-    
 
     $('.collapsible').collapsible();
 
+    $('.tooltipped').tooltip();
 
+    $('.anchor-list').on('click', function(evt) {
+        evt.preventDefault();
+        console.log($(this).attr('href'));
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 1000);
+    });
 
-// TYPE FORM 
-    (function () {
-        var qs, js, q, s, d = document,
+    // TYPE FORM
+    (function() {
+        var qs,
+            js,
+            q,
+            s,
+            d = document,
             gi = d.getElementById,
             ce = d.createElement,
             gt = d.getElementsByTagName,
-            id = "typef_orm",
-            b = "https://embed.typeform.com/";
+            id = 'typef_orm',
+            b = 'https://embed.typeform.com/';
         if (!gi.call(d, id)) {
-            js = ce.call(d, "script");
+            js = ce.call(d, 'script');
             js.id = id;
-            js.src = b + "embed.js";
-            q = gt.call(d, "script")[0];
-            q.parentNode.insertBefore(js, q)
+            js.src = b + 'embed.js';
+            q = gt.call(d, 'script')[0];
+            q.parentNode.insertBefore(js, q);
         }
-    })()
-
+    })();
 });
-
-
