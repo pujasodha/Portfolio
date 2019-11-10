49a1e34d3dcec10835dc313238a5ce5a61a564fb
pujasodha <pujasodha@gmail.com>
2019-11-10 00:55:55 -0800

    animations + responsiveness

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,25 +14,4 @@ $(document).ready(function() {
         console.log($(this).attr('href'));
         $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 1000);
     });
-
-    // TYPE FORM
-    (function() {
-        var qs,
-            js,
-            q,
-            s,
-            d = document,
-            gi = d.getElementById,
-            ce = d.createElement,
-            gt = d.getElementsByTagName,
-            id = 'typef_orm',
-            b = 'https://embed.typeform.com/';
-        if (!gi.call(d, id)) {
-            js = ce.call(d, 'script');
-            js.id = id;
-            js.src = b + 'embed.js';
-            q = gt.call(d, 'script')[0];
-            q.parentNode.insertBefore(js, q);
-        }
-    })();
 });
